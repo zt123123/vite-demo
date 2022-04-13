b777b193a084b959b619b3022e4ff39539252497
zhangtian <zhangtian@weidian.com>
2022-04-13 16:00:37 +0800

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vuePlugin from "@vitejs/plugin-vue"
 
 export default defineConfig({
   publicDir: "./public",
-  plugins: [(vuePlugin())]
+  plugins: [(vuePlugin())],
+  base: 'https://zt123123.github.io/vite-demo/'
 })
